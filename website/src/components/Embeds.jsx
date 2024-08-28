4a2b136b910151644e779919cc61924425fd5c8c
Jim <hippiejim2896@gmail.com>
2024-08-28 00:51:42 -0500

    Fix list in embeds
    Co-authored-by: bgilross <bgilross@users.noreply.github.com>

--- website/src/components/Embeds.jsx
+++ website/src/components/Embeds.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Embeds = () => {
   // const video1ID = 'B8N9qrY00Ww?si=mVlcgESyhGfiXBxT'
@@ -7,52 +7,53 @@ const Embeds = () => {
   // const video4ID = 'Z9f-nLc71nY?si=hJOqsZdocrMH1hZJ'
   // const video5ID = 'bAYzAUcurik?si=ZpUqU48NjdHKy2bc'
 
-  const video1ID = 'B8N9qrY00Ww'
-  const video2ID = 'HV4P7Xx7SWU'
-  const video3ID = 'r0Th11HFyC0'
-  const video4ID = 'Z9f-nLc71nY'
-  const video5ID = 'bAYzAUcurik'
-  const [featuredVideoId, setFeaturedVideoId] = useState('B8N9qrY00Ww')
+  const video1ID = "B8N9qrY00Ww";
+  const video2ID = "HV4P7Xx7SWU";
+  const video3ID = "r0Th11HFyC0";
+  const video4ID = "Z9f-nLc71nY";
+  const video5ID = "bAYzAUcurik";
+  const [featuredVideoId, setFeaturedVideoId] = useState("B8N9qrY00Ww");
   const [thumbnails, setThumbnails] = useState([
+    { id: video1ID },
     { id: video2ID },
     { id: video3ID },
     { id: video4ID },
     { id: video5ID },
-  ])
+  ]);
 
   const handleThumbnailClick = (index) => {
     // Swap the featured video with the clicked thumbnail
-    const newFeaturedVideoId = thumbnails[index].id
+    const newFeaturedVideoId = thumbnails[index].id;
     const newThumbnails = thumbnails.map((thumbnail, i) =>
       i === index ? { id: featuredVideoId } : thumbnail
-    )
-    setFeaturedVideoId(newFeaturedVideoId)
-    setThumbnails(newThumbnails)
-  }
+    );
+    setFeaturedVideoId(newFeaturedVideoId);
+    setThumbnails(newThumbnails);
+  };
 
   return (
-    <div className="max-w-4xl h-full mx-auto mt-12 text-center flex flex-col">
+    <div className='max-w-4xl h-full mx-auto mt-12 text-center flex flex-col'>
       {/* Featured Video */}
-      <div className="mb-4">
+      <div className='mb-4'>
         <iframe
-          className="vid"
+          className='vid'
           src={`https://www.youtube.com/embed/${featuredVideoId}?autoplay=1`}
-          frameBorder="0"
-          allow="autoplay; encrypted-media"
+          frameBorder='0'
+          allow='autoplay; encrypted-media'
           allowFullScreen
         ></iframe>
       </div>
 
       {/* Thumbnails Row */}
-      <div className="flex justify-center space-x-4">
+      <div className='flex justify-center space-x-4'>
         {thumbnails.map((thumbnail, index) => (
           <div
             key={index}
-            className="vids"
+            className='vids'
             onClick={() => handleThumbnailClick(index)}
           >
             <img
-              className="w-40 h-24 md:w-56 md:h-32 object-cover"
+              className='w-40 h-24 md:w-56 md:h-32 object-cover'
               src={`https://img.youtube.com/vi/${thumbnail.id}/hqdefault.jpg`}
               alt={`Thumbnail of video ${thumbnail.id}`}
             />
@@ -60,7 +61,7 @@ const Embeds = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Embeds
+export default Embeds;
