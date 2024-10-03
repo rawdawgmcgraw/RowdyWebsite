06f5491257460d6ef9524b3d3a46ac60a08b167b
Jim <hippiejim2896@gmail.com>
2024-10-03 16:13:09 -0500

    Remove unused assets Change image on Hero mobile view

--- website/src/components/Hero.jsx
+++ website/src/components/Hero.jsx
@@ -1,26 +1,26 @@
-import mobilePic from '../images/mobilePic.jpg'
-import Popup1 from './Popup1'
-import Social from './Social'
+import bckgrndImage1 from "../images/bkgrndImage1.png";
+import Popup1 from "./Popup1";
+import Social from "./Social";
 const Hero = () => {
   return (
-    <div className="h-full w-full">
-      <div className="md:hidden h-full flex justify-center flex-col items-center ">
+    <div className='h-full w-full'>
+      <div className='md:hidden h-full flex justify-center flex-col items-center '>
         <img
-          className="h-[50%] rounded-3xl shadow-md shadow-purple-800 opacity-80"
-          src={mobilePic}
-          alt="/"
+          className='h-[60%] rounded-3xl shadow-md shadow-purple-800 opacity-80'
+          src={bckgrndImage1}
+          alt='/'
         />
-        <div className="pt-16">
+        <div className='pt-16'>
           <Popup1 />
         </div>
       </div>
-      <div className="hidden md:flex h-full w-full items-center">
-        <div className="flex flex-col items-center justify-end pb-16 w-full h-full bg-background bg-contain bg-no-repeat bg-center mix-blend-screen">
+      <div className='hidden md:flex h-full w-full items-center'>
+        <div className='flex flex-col items-center justify-end pb-16 w-full h-full bg-background bg-contain bg-no-repeat bg-center mix-blend-screen'>
           <Social />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
